ff6ed7eef1a6160294b5866ad867a3ea07396c5b
NPPortfolio <11758845+NPPortfolio@users.noreply.github.com>
2021-07-29 19:27:45 -0400

    small image file reading
    -

--- python_test.py
+++ python_test.py
@@ -5,11 +5,23 @@ import PIL
 import PIL.Image
 import pathlib
 
-image_folder = pathlib.Path('./data/images')
-images = list(image_folder.glob('*.jpg'))
+# image_folder = pathlib.Path('./data/images')
+# images = list(image_folder.glob('*.jpg'))
 
-print(len(images))
+list_ds = tf.data.Dataset.list_files('./data/images/*.jpg', shuffle=False)
 
-batch_size = 32
-img_height = 150
-img_width = 150
+image_count = len(list_ds)
+
+# Tensorflow tutorial functions
+def img_from_path(file_path):
+    img = tf.io.read_file(file_path)
+    return decode_img(img)
+
+def decode_img(img):
+    # convert the compressed string to a 3D uint8 tensor
+    return tf.io.decode_jpeg(img, channels=3)
+
+for e in list_ds.take(1):
+    print(img_from_path(e))
+
+
