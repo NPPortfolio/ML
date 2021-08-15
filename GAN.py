a4d55159338635a04b6e8df96e8857f08c02bca2
NPPortfolio <11758845+NPPortfolio@users.noreply.github.com>
2021-08-15 16:38:10 -0400

    tutorial code
    -

--- GAN.py
+++ GAN.py
@@ -1,6 +1,13 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import time
+
 from tensorflow.keras import layers
+
+# https://www.tensorflow.org/tutorials/generative/dcgan
+# Right now this file is mostly the code from the above tutorial
+# As I go I will tweak to fit with the landscape generator ideas
+
 
 def create_discriminator():
 
@@ -8,7 +15,7 @@ def create_discriminator():
 
     model = tf.keras.Sequential()
     model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
-                                     input_shape=[256, 256, 3]))
+                            input_shape=[256, 256, 3]))
     model.add(layers.LeakyReLU())
     model.add(layers.Dropout(0.3))
 
@@ -20,6 +27,7 @@ def create_discriminator():
     model.add(tf.keras.layers.Dense(1))
 
     return model
+
 
 def create_generator():
 
@@ -35,31 +43,37 @@ def create_generator():
     # NOTE: I think this input shape is where each label has to have some influence
     model = tf.keras.Sequential()
     model.add(layers.Dense(64 * 32 * 32, input_shape=(100,), activation='relu'))
-    model.add(layers.BatchNormalization()) # Transformation that maintains the mean output close to 0 and SD close to 1
-    model.add(layers.LeakyReLU(alpha=0.2)) # Question whether to have this before or after activation function
+    # Transformation that maintains the mean output close to 0 and SD close to 1
+    model.add(layers.BatchNormalization())
+    # Question whether to have this before or after activation function
+    model.add(layers.LeakyReLU(alpha=0.2))
 
-    model.add(layers.Reshape((32, 32, 64))) # Question about rbg channels here
+    model.add(layers.Reshape((32, 32, 64)))  # Question about rbg channels here
     print(model.output.shape)
 
-    model.add(layers.Conv2DTranspose(32, (8, 8), strides=(2,2), padding='same', use_bias=False)) # 32 64 x 64 filters
+    model.add(layers.Conv2DTranspose(32, (8, 8), strides=(2, 2),
+              padding='same', use_bias=False))  # 32 64 x 64 filters
     print(model.output.shape)
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU(alpha=0.2))
 
-    model.add(layers.Conv2DTranspose(16, (12, 12), strides=(2,2), padding='same', use_bias=False)) # 16 128 * 128 filters
+    model.add(layers.Conv2DTranspose(16, (12, 12), strides=(2, 2),
+              padding='same', use_bias=False))  # 16 128 * 128 filters
     print(model.output.shape)
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU(alpha=0.2))
 
     # Important note: probably want more than one filter here, next layer defines final image
-    model.add(layers.Conv2DTranspose(16, (16, 16), strides=(2,2), padding='same', use_bias=False)) # 16 256 * 256 filters
+    model.add(layers.Conv2DTranspose(16, (16, 16), strides=(2, 2),
+              padding='same', use_bias=False))  # 16 256 * 256 filters
     print(model.output.shape)
 
-    # This output layer has 3 256 * 256 filters (rgb), tanh makes values between -1 and 1 
-    model.add(layers.Conv2D(3, (3,3), activation='tanh', padding='same'))
+    # This output layer has 3 256 * 256 filters (rgb), tanh makes values between -1 and 1
+    model.add(layers.Conv2D(3, (3, 3), activation='tanh', padding='same'))
     print(model.output.shape)
 
     return model
+
 
 generator = create_generator()
 
@@ -69,32 +83,82 @@ generated_image = generator(noise, training=False)
 plt.imshow(generated_image[0, :, :, 0], cmap='gray')
 plt.show()
 
+discriminator = create_discriminator()
+
+# This method returns a helper function to compute cross entropy loss
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+#
 
 
+def discriminator_loss(real_output, fake_output):
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
 
 
+def generator_loss(fake_output):
+    return cross_entropy(tf.ones_like(fake_output), fake_output)
 
 
+# Each network trained separately, different optimizers
+generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+
+BATCH_SIZE = 1
+EPOCHS = 1
+noise_dim = 100
+num_examples_to_generate = 16
 
 
+@tf.function  # This causes function to be "compiled", from tutorial
+def train_step(images):
+
+    # Random values in normal distribution between batch size and noise_dim
+    noise = tf.random.normal([BATCH_SIZE, noise_dim])
+
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        generated_images = generator(noise, training=True)
+
+        real_output = discriminator(images, training=True)
+        fake_output = discriminator(generated_images, training=True)
+
+        gen_loss = generator_loss(fake_output)
+        disc_loss = discriminator_loss(real_output, fake_output)
+
+    gradients_of_generator = gen_tape.gradient(
+        gen_loss, generator.trainable_variables)
+    gradients_of_discriminator = disc_tape.gradient(
+        disc_loss, discriminator.trainable_variables)
+
+    # zip function organizes gradients and variables into tuples
+    generator_optimizer.apply_gradients(
+        zip(gradients_of_generator, generator.trainable_variables))
+    discriminator_optimizer.apply_gradients(
+        zip(gradients_of_discriminator, discriminator.trainable_variables))
 
 
-# tutorial source for later
-# https://machinelearningmastery.com/how-to-develop-a-generative-adversarial-network-for-a-cifar-10-small-object-photographs-from-scratch/
+def train(dataset, epochs):
 
-# Probably not going to use
+    for epoch in range(epochs):
+        start = time.time()
 
-def create_gan(dis, gen):
-    # make weights in the discriminator not trainable
-    # From the tutorial, this means that the standalone discriminator can be trained but the wieghts will not update when GAN is trained?
-	dis.trainable = False
-	# connect them
-	model = tf.keras.layers.Sequential()
-	# add generator
-	model.add(gen)
-	# add the discriminator
-	model.add(dis)
-	# compile model
-	opt = tf.keras.optimizers.Adam(lr=0.0002, beta_1=0.5)
-	model.compile(loss='binary_crossentropy', optimizer=opt)
-	return model
+        for image_batch in dataset:
+            train_step(image_batch)
+
+        print('Time for epoch {} is {} sec'.format(
+            epoch + 1, time.time()-start))
+
+
+def generate_images(model, test_input):
+    # Notice `training` is set to False.
+    # This is so all layers run in inference mode (batchnorm).
+    predictions = model(test_input, training=False)
+
+    for i in range(predictions.shape[0]):
+        plt.subplot(4, 4, i+1)
+        plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
+        plt.axis('off')
+
+    plt.show()
